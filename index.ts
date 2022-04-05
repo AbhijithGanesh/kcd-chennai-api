bf99828f139870de909f39e643b18e929dfedefe
Abhijith Ganesh <67182544+AbhijithGanesh@users.noreply.github.com>
2022-04-06 00:35:26 +0530

    Init (#1)
    * Addition of KCD-api
    * Removal of API Key
    * Update index.ts
    Signed-off-by: Abhijith Ganesh <67182544+AbhijithGanesh@users.noreply.github.com>

---
+++ index.ts
@@ -0,0 +1,28 @@
+import axios, { AxiosResponse } from "axios";
+
+const Iterator = (data: any) => {
+  for (let i = 0; i < data.length; i++) {}
+};
+
+const organization_articles_user_wise = (user: String) => {
+  const base_url: string = "https://dev.to";
+};
+
+const All_articles_organization = (org_name: String) => {
+  const base_url: string = "https://dev.to";
+  const config = { "api-key": "" };
+  axios
+    .get(`${base_url}/api/organizations/${org_name}/articles`, {
+      headers: config,
+    })
+    .then((res: AxiosResponse) => {
+      let data:Object = res.data;
+      let leader_board = {};
+      for (let i = 0; i < 2; i++) {
+        if (leader_board[data[i]["user"]["name"]] != "undefined")
+      }
+
+    });
+};
+
+All_articles_organization("org-name"); /// Enter your organization Name
